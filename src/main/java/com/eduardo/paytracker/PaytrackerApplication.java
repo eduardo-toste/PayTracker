88bb4817fc8e0f28c66ca1b86eec6c971d4bab67
eduardo-toste <duciochetti@gmail.com>
2025-04-10 17:25:39 -0300

    initializing project

---
+++ src/main/java/com/eduardo/paytracker/PaytrackerApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.paytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaytrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo/paytracker/PaytrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.paytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
