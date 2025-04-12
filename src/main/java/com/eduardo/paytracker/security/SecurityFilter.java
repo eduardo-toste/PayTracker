68d3994a8f9bfc89ec1aa4a117fa3858f0c398eb
eduardo-toste <duciochetti@gmail.com>
2025-04-12 18:12:56 -0300

    feat: create security configuration and filter

---
+++ src/main/java/com/eduardo/paytracker/security/SecurityFilter.java
@@ -0,0 +1,48 @@
+package com.eduardo.paytracker.security;
+
+import com.eduardo.paytracker.repository.UserRepository;
+import com.eduardo.paytracker.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = getToken(request);
+
+        if(token != null){
+            var subject = tokenService.validateToken(token);
+            var user = userRepository.findByEmail(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+    }
+
+    private String getToken(HttpServletRequest request){
+        var authoritzationHeader = request.getHeader("Authoritzation");
+
+        if(authoritzationHeader != null){
+            return authoritzationHeader.replace("Bearer ", "");
+        }
+
+        return null;
+    }
+}
