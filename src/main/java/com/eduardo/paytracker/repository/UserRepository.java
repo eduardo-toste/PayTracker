3d90ea879910f3d1d682c908aeb87a6cf2890517
eduardo-toste <duciochetti@gmail.com>
2025-04-12 17:29:11 -0300

    feat: create method to find user by email

--- src/main/java/com/eduardo/paytracker/repository/UserRepository.java
+++ src/main/java/com/eduardo/paytracker/repository/UserRepository.java
@@ -2,6 +2,10 @@ package com.eduardo.paytracker.repository;
 
 import com.eduardo.paytracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByEmail(String username);
+
 }
