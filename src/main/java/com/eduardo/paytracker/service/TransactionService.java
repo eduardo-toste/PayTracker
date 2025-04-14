eea7dd63e7e8787c306bd1b6bc470038000d519a
eduardo-toste <duciochetti@gmail.com>
2025-04-14 11:15:35 -0300

    feat: create transaction service class

---
+++ src/main/java/com/eduardo/paytracker/service/TransactionService.java
@@ -0,0 +1,19 @@
+package com.eduardo.paytracker.service;
+
+import com.eduardo.paytracker.dto.TransactionRequestDTO;
+import com.eduardo.paytracker.dto.TransactionResponseDTO;
+import com.eduardo.paytracker.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    public TransactionResponseDTO createTransaction(TransactionRequestDTO data) {
+
+        return new TransactionResponseDTO(null, null, null,null,null,null, null);
+    }
+}
