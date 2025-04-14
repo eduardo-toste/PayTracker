d2b8cfae1b38baa604902cd2bb6a108737f66051
eduardo-toste <duciochetti@gmail.com>
2025-04-14 18:03:01 -0300

    feat: create get all transactions endpoint

--- src/main/java/com/eduardo/paytracker/controller/TransactionController.java
+++ src/main/java/com/eduardo/paytracker/controller/TransactionController.java
@@ -5,9 +5,13 @@ import com.eduardo.paytracker.dto.TransactionResponseDTO;
 import com.eduardo.paytracker.service.TransactionService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/transaction")
@@ -21,6 +25,13 @@ public class TransactionController {
         var createdTransaction = transactionService.createTransaction(data);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(createdTransaction);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<TransactionResponseDTO>> getAll(Pageable pageable){
+        var transactions = transactionService.getAllTransactions(pageable);
+
+        return ResponseEntity.status(HttpStatus.OK).body(transactions);
     }
 
 }
