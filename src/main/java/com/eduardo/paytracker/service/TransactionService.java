f889962e4c1b01c8b49e3f1ffde7640f7fbd3e4b
eduardo-toste <duciochetti@gmail.com>
2025-04-14 13:06:54 -0300

    feat: create get user method for user id in transaction

--- src/main/java/com/eduardo/paytracker/service/TransactionService.java
+++ src/main/java/com/eduardo/paytracker/service/TransactionService.java
@@ -1,10 +1,18 @@
 package com.eduardo.paytracker.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.eduardo.paytracker.dto.TransactionRequestDTO;
 import com.eduardo.paytracker.dto.TransactionResponseDTO;
+import com.eduardo.paytracker.model.Transaction;
+import com.eduardo.paytracker.model.User;
 import com.eduardo.paytracker.repository.TransactionRepository;
+import com.eduardo.paytracker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class TransactionService {
@@ -12,8 +20,39 @@ public class TransactionService {
     @Autowired
     private TransactionRepository transactionRepository;
 
-    public TransactionResponseDTO createTransaction(TransactionRequestDTO data) {
+    @Autowired
+    private UserRepository userRepository;
 
-        return new TransactionResponseDTO(null, null, null, null,null,null,null, null);
+    public TransactionResponseDTO createTransaction(TransactionRequestDTO data) {
+        var user = getUser();
+
+        var transaction = new Transaction(
+                null,
+                data.title(),
+                data.description(),
+                data.amount(),
+                LocalDate.now(),
+                data.dueDate(),
+                data.type(),
+                user
+        );
+
+        var savedTransaction = transactionRepository.save(transaction);
+
+        return new TransactionResponseDTO(savedTransaction);
     }
+
+    private User getUser() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String token = (String) authentication.getCredentials();
+        DecodedJWT decodedJWT = JWT.decode(token);
+        String email = decodedJWT.getClaim("sub").asString();
+
+        User user = (User) userRepository.findByEmail(email);
+        Long userId = user.getId();
+
+        return userRepository.getReferenceById(userId);
+    }
+
 }
