deaf978e9b6c7528b31263ec532f1627c219f219
eduardo-toste <duciochetti@gmail.com>
2025-04-14 13:07:30 -0300

    refactor: update create method to public

--- src/main/java/com/eduardo/paytracker/controller/TransactionController.java
+++ src/main/java/com/eduardo/paytracker/controller/TransactionController.java
@@ -2,16 +2,12 @@ package com.eduardo.paytracker.controller;
 
 import com.eduardo.paytracker.dto.TransactionRequestDTO;
 import com.eduardo.paytracker.dto.TransactionResponseDTO;
-import com.eduardo.paytracker.repository.TransactionRepository;
 import com.eduardo.paytracker.service.TransactionService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transaction")
@@ -21,7 +17,7 @@ public class TransactionController {
     private TransactionService transactionService;
 
     @PostMapping
-    private ResponseEntity<TransactionResponseDTO> create(@RequestBody @Valid TransactionRequestDTO data){
+    public ResponseEntity<TransactionResponseDTO> create(@RequestBody @Valid TransactionRequestDTO data){
         var createdTransaction = transactionService.createTransaction(data);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(createdTransaction);
