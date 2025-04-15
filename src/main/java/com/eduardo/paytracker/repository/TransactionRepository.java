56487dcaf90180c489a335714a62b34747fa9f38
eduardo-toste <duciochetti@gmail.com>
2025-04-14 22:49:07 -0300

    feat: create transaction find methods

--- src/main/java/com/eduardo/paytracker/repository/TransactionRepository.java
+++ src/main/java/com/eduardo/paytracker/repository/TransactionRepository.java
@@ -1,7 +1,17 @@
 package com.eduardo.paytracker.repository;
 
 import com.eduardo.paytracker.model.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    @Query("select t from Transaction t where t.user.id = :userId")
+    Page<Transaction> findAllTransactions(Long userId, Pageable pageable);
+
+    @Query("select t from Transaction t where t.id = :transactionId and t.user.id = :userId")
+    Transaction findTransactionById(Long userId, Long transactionId);
+
 }
