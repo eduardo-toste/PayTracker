9226ebc5d09e877b9f852049533241f87878dda2
eduardo-toste <duciochetti@gmail.com>
2025-04-15 19:35:00 -0300

    feat: create custom token exception

---
+++ src/main/java/com/eduardo/paytracker/exception/TokenException.java
@@ -0,0 +1,9 @@
+package com.eduardo.paytracker.exception;
+
+public class TokenException extends RuntimeException {
+
+    public TokenException(String message) {
+        super(message);
+    }
+
+}
