334cbedddf83aa16f6ad29d5a4df4e2eaa416cce
eduardo-toste <duciochetti@gmail.com>
2025-04-16 00:36:22 -0300

    feat: create update methods

--- src/main/java/com/eduardo/paytracker/model/Transaction.java
+++ src/main/java/com/eduardo/paytracker/model/Transaction.java
@@ -1,5 +1,7 @@
 package com.eduardo.paytracker.model;
 
+import com.eduardo.paytracker.dto.TransactionPatchRequestDTO;
+import com.eduardo.paytracker.dto.TransactionRequestDTO;
 import com.eduardo.paytracker.model.enums.TransactionType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -48,5 +50,21 @@ public class Transaction {
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    public void updateFrom(TransactionRequestDTO data) {
+        this.title = data.title();
+        this.description = data.description();
+        this.amount = data.amount();
+        this.dueDate = data.dueDate();
+        this.type = data.type();
+    }
+
+    public void updateFromPatch(TransactionPatchRequestDTO data) {
+        if (data.title() != null) this.title = data.title();
+        if (data.description() != null) this.description = data.description();
+        if (data.amount() != null) this.amount = data.amount();
+        if (data.dueDate() != null) this.dueDate = data.dueDate();
+        if (data.type() != null) this.type = data.type();
+    }
 
 }
